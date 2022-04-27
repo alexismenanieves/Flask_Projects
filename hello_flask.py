de09c90ce5e72a0bda35c745e85f61efb0faafd8
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-27 17:29:40 -0500

    Added comments

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,14 @@
+# We'll use Flask
 from flask import Flask
 
+# To create an app instance that points to our file using the name
 app = Flask(__name__)
 
+# The we use a decorator to indicate in what route we serve
 @app.route('/')
 def hello_flask():
   return 'Hello Flask!!'
 
+# Then we run the app. We'll get a warning that this is dev server
 if __name__ == '__main__':
   app.run()
--- hello_flask_jsonify.py
+++ hello_flask_jsonify.py
@@ -1,9 +1,11 @@
+# Now if we want to create a RESTful API, we return a JSON
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_flask_jsonify():
+  # jsonify convert our message into a key a value pair JSON
   return jsonify(message='Hello Flask & jsonify!')
 
 if __name__ == '__main__':
