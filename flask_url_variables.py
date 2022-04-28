c75afbbe8e56191946305bd95f7ff2eeae362acf
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-27 21:39:08 -0500

    Added example of URL variables

--- flask_url_variables.py
+++ flask_url_variables.py
@@ -1,5 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def show_home():
+  return render_template('index.html')
+
+# You can pass variables as url strings
 @app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+  if age < 18:
+    return jsonify(message = 'We are sorry ' + name + ' not authorized'), 401
+  else:
+    return jsonify(message = 'Welcome back ' + name), 200
+
+if __name__ == '__main__':
+  app.run(debug=True, host='127.0.0.1', port=5000)
