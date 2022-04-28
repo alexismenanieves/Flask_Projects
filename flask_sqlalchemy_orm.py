5ac64eeae683f93dc32c7cd5c0ee91998a9b1fb6
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-28 10:22:41 -0500

    To improve readability, I separated long sentences. Added references and comments about tracking modifications

--- flask_sqlalchemy_orm.py
+++ flask_sqlalchemy_orm.py
@@ -4,7 +4,9 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'gapminder.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' \
+    + os.path.join(basedir, 'gapminder.sqlite')
+# See reference 1. Tracking modifications adds overhead, better deactivate 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -14,3 +16,7 @@ def show_home():
 
 if __name__ == '__main__':
   app.run(debug=True, host='127.0.0.1', port=5000)
+
+# REFERENCES
+# 1. About Track modifications: https://stackoverflow.com/questions/61573598/
+
