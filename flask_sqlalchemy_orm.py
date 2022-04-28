9e3e60c26b8469a2d3e1e7d57003ce306d4b19eb
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-27 22:21:18 -0500

    Added a false state for track modifications

--- flask_sqlalchemy_orm.py
+++ flask_sqlalchemy_orm.py
@@ -1,3 +1,16 @@
 from flask import Flask, jsonify, render_template
-from flask_alchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'gapminder.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def show_home():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True, host='127.0.0.1', port=5000)
