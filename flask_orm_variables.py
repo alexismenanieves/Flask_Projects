6c35c40c08d2a7bc4b9758a78f4dc1ac2ccb7603
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-28 15:07:05 -0500

    Template for API with variable call and orm

---
+++ flask_orm_variables.py
@@ -0,0 +1,15 @@
+# Section 0. Load libraries and custom functions --------------------------
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Section 1. Initialize Flask app -----------------------------------------
+
+# Section 2. Data Classes -------------------------------------------------
+
+# Section 3. Serializations -----------------------------------------------
+
+# Section 4. Routes -------------------------------------------------------
+
+# Section 5. Main and App settings ----------------------------------------
