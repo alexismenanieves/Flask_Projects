ef1f891f3e0f62b9c1cd730974648d5a93286a4f
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-28 15:32:29 -0500

    Create connection

--- flask_orm_variables.py
+++ flask_orm_variables.py
@@ -5,11 +5,17 @@ from flask_marshmallow import Marshmallow
 import os
 
 # Section 1. Initialize Flask app -----------------------------------------
+app = Flask(__name__)
 
 # Section 2. Data Classes -------------------------------------------------
+basedir = os.path.abspath(os.path.dirname(__name__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///' \
+    + os.path.join(basedir,'gapminder.sqlite') 
 
 # Section 3. Serializations -----------------------------------------------
 
 # Section 4. Routes -------------------------------------------------------
 
 # Section 5. Main and App settings ----------------------------------------
+if __name__ == '__main__':
+  app.run(debug=True, host='127.0.0.1', port=5000)
