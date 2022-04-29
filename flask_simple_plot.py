2cb39ec30ecf05436b801fd43e823b233307a09b
alexismenanieves <alexismenanieves@hotmail.com>
2022-04-28 22:35:06 -0500

    Created route to display a Plotly plot

--- flask_simple_plot.py
+++ flask_simple_plot.py
@@ -1,8 +1,24 @@
 from flask import Flask, jsonify, render_template
 import plotly
-import plotly.graph_objs as pgo
+import plotly.graph_objs as go
 import json
 
 app = Flask(__name__)
 
-@
+@app.route('/simple_plot')
+def show_simple_plot():
+  bar = create_plot()
+  return render_template('plots.html', plot=bar)
+
+def create_plot():
+  data = [
+    go.Bar(
+      x = ['Hugo','Paco','Luis'],
+      y = [15, 5, 19]
+    )
+  ]
+  
+  graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+  return graphJSON
+
+
